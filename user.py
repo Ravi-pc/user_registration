0f804eec323576bff4da4ea04ff5aa99b2e0b76f
“Ravi-pc” <ravisingh98138@gmail.com>
2023-12-20 14:40:27 +0530

    [Ravi] Add: program to check the last name of the user

--- user.py
+++ user.py
@@ -14,12 +14,13 @@ import re
 
 
 class User:
-    def __init__(self, first_name):
+    def __init__(self, first_name, last_name):
         self.first_name = first_name
+        self.last_name = last_name
 
     def name_validation(self):
         check = re.compile(r'^[A-Z][a-zA-Z]{2,}$')
-        if check.match(self.first_name):
+        if check.match(self.first_name) and check.match(self.last_name):
             return True
         else:
             return False
@@ -27,5 +28,6 @@ class User:
 
 if __name__ == '__main__':
     f_name = input('Enter First Name\n')
-    user_obj = User(f_name)
+    l_name = input('Enter Last Name\n')
+    user_obj = User(f_name, l_name)
     print(user_obj.name_validation())
